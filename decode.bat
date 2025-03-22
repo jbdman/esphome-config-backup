7b7964a0d1ec19f3a184174c71afb4efcd7ddf37
Josh <jbdelutis@gmail.com>
2025-03-22 09:48:14 -0400

    Update decoder & examples

--- decode.bat
+++ decode.bat
@@ -1,2 +1,2 @@
 python decode.py --encryption xor --key mysecretkey example-config-xor-mysecretkey 
-python decode.py --encryption aes256 --key 1234567812345678 example-config-aes256-1234567812345678
+python decode.py --encryption aes256 --key mysecretkey --salt espsalt example-config-aes256-mysecretkey-espsalt
