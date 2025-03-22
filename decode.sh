420d82d748aa125a1d5c50b84d93428883df9c0b
Josh <jbdelutis@gmail.com>
2025-03-22 09:56:31 -0400

    Update decode.sh

--- decode.sh
+++ decode.sh
@@ -1,1 +1,3 @@
-decode.bat
+#!/bin/bash
+python decode.py --encryption xor --key mysecretkey example-config-xor-mysecretkey 
+python decode.py --encryption aes256 --key mysecretkey example-config-aes256-mysecretkey
