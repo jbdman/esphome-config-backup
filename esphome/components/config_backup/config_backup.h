bc9a3e409ee9f6a64661a553e943f4efa8abe62c
Josh <jbdelutis@gmail.com>
2025-03-23 15:54:37 -0400

    Update config_backup.h

--- esphome/components/config_backup/config_backup.h
+++ esphome/components/config_backup/config_backup.h
@@ -76,6 +76,7 @@ class ConfigBackup : public esphome::Component, public AsyncWebHandler {
   void handleRequest(AsyncWebServerRequest *request) override {
     if (request->url() == "/config.b64") {
       AsyncWebServerResponse *response = request->beginResponse_P(200, "text/plain", CONFIG_B64, CONFIG_B64_SIZE);
+      response->addHeader("Content-Encoding", "gzip");
       response->addHeader("X-Encryption-Type", this->encryption);
       request->send(response);
     }
