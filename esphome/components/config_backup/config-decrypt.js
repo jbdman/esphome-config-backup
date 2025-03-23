33a78f3a54cc1bdf4cd35fc43b10361ed8cdaa9c
Josh <jbdelutis@gmail.com>
2025-03-23 14:21:56 -0400

    Update config-decrypt.js

--- esphome/components/config_backup/config-decrypt.js
+++ esphome/components/config_backup/config-decrypt.js
@@ -134,7 +134,7 @@ async function injectConfigBackupWidget() {
     wrapper.innerHTML = `
         <h2>Config Backup</h2>
         <form id="config-backup-form">
-          <input id="decrypt-key" placeholder="Key" /> 
+          <input id="decrypt-key" placeholder="Key" />&nbsp;
           <input class="btn" type="submit" value="Decrypt">
         </form>
         <pre id="decrypt-output" style="white-space: pre-wrap; margin-top: 10px;"></pre>
