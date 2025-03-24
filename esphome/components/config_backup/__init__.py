27eb5c620a306108f9ad085745f587432bdb42cf
Josh <jbdelutis@gmail.com>
2025-03-24 18:32:28 -0400

    Update __init__.py

--- esphome/components/config_backup/__init__.py
+++ esphome/components/config_backup/__init__.py
@@ -135,6 +135,7 @@ def embedFile(
         for old, new in placeholder_replace.items():
             logger.info(f"Replacing {old} with {new}...")
             text_str = text_str.replace(old, new)
+        logger.info(text_str)
         data = text_str.encode('utf-8')
 
     if mangle:
