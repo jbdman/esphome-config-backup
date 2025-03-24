b054e6669a59bba8efcb4db4be5bdf1a4b88d2ff
Josh <jbdelutis@gmail.com>
2025-03-24 19:57:36 -0400

    Update globalv.py

--- bin/Python/globalv.py
+++ bin/Python/globalv.py
@@ -1,13 +1,32 @@
 from types import SimpleNamespace as sn
+
 from cryptography.hazmat.primitives import padding as aes_padding
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import modes #Cipher, algorithms, 
+#from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+#from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 aes = sn(
 	padder=sn(
-		python=aes_padding.PKCS7(128).padder(),
+		python=aes_padding.PKCS7,
 		javascript="CryptoJS.pad.Pkcs7"
 	),
+	mode=sn(
+		python=modes.CBC,
+		javascript="CryptoJS.mode.CBC"
+	),
+	PBKDF2=sn(
+		algorithm=sn(
+			python=hashes.SHA256,
+			javascript="CryptoJS.algo.SHA256"
+		),
+		iterations=sn(
+			python=100_000,
+			javascript="100000"
+		),
+		length=sn(
+			python=32,
+			javascript="32"
+		)
+	)
 )
