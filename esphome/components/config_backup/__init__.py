816e6551ca72e8414cad6137bb45adaa2026f7d3
Josh <jbdelutis@gmail.com>
2025-03-24 20:16:03 -0400

    Update __init__.py

--- esphome/components/config_backup/__init__.py
+++ esphome/components/config_backup/__init__.py
@@ -78,10 +78,10 @@ import gzip
 import hashlib
 from cryptography.fernet import Fernet
 # We now switch fully to the "cryptography" library for AES:
-from cryptography.hazmat.primitives import padding as aes_padding
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+#from cryptography.hazmat.primitives import padding as aes_padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms #, modes
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
+#from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 
