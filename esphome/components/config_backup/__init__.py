2df28fa953363e3d9ab50e720b45501237fc9392
Josh <jbdelutis@gmail.com>
2025-03-24 21:40:45 -0400

    Update __init__.py

--- esphome/components/config_backup/__init__.py
+++ esphome/components/config_backup/__init__.py
@@ -75,13 +75,10 @@ import uglify_wrapper
 # Embed logic (compression, encryption, placeholder replacement, etc.).
 # --------------------------------------------------------------------
 import gzip
-# import hashlib
-# from cryptography.fernet import Fernet
+
 # We now switch fully to the "cryptography" library for AES:
-#from cryptography.hazmat.primitives import padding as aes_padding
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms #, modes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms
 from cryptography.hazmat.backends import default_backend
-#from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 
