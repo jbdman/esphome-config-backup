32a52770a983e146ba5284eb5dcf3905e6926ce6
Josh <jbdelutis@gmail.com>
2025-03-24 21:40:43 -0400

    Update decode.py

--- bin/Python/decode.py
+++ bin/Python/decode.py
@@ -6,10 +6,8 @@ import gzip
 import globalv
 
 # We now switch fully to the "cryptography" library for AES:
-#from cryptography.hazmat.primitives import padding as aes_padding
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms #, modes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms
 from cryptography.hazmat.backends import default_backend
-#from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 
