a82081e919eec5ec48d5e11be3690fce690470c3
Josh <jbdelutis@gmail.com>
2025-03-26 19:31:28 -0400

    Update compression.py

--- esphome/components/config_backup/compression.py
+++ esphome/components/config_backup/compression.py
@@ -1,1 +1,15 @@
-compression.py
+import gzip
+
+class Compressor:
+
+	def __init__(self, type: str):
+		self.type = type
+		if self.type == 'gzip':
+			self.compress = gzip.compress
+			self.decompress = gzip.decompress
+		elif self.type == 'none':
+			self.compress = self._none
+			self.decompress = self._none
+
+	def _none(self, data: bytes) -> bytes:
+		return data
