268b7f08ad1cb2c4885f5547b8b8377d0ce35bca
Josh <jbdelutis@gmail.com>
2025-03-26 20:30:01 -0400

    Update encryption.py

--- esphome/components/config_backup/encryption.py
+++ esphome/components/config_backup/encryption.py
@@ -4,7 +4,7 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import secrets
-import globalv
+from . import globalv
 
 class Encryptor:
 
